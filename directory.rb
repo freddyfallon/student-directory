c9e31e92673144e6ad301390d251bbaa9e14e1c0
Freddy Fallon <freddyfallon@gmail.com>
2017-02-28 12:07:47 +0000

    Refactoring directory.rb

--- directory.rb
+++ directory.rb
@@ -1,61 +1,66 @@
 # let's put all the students into an array
-
+@students = []
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
-def print(students)
-  students.each do |student|
+def print_student_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def footer(students)
-  puts "Overall, we have #{students.count} great students"
+def footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
 def input_statements
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
   # get the first name
   name = $stdin.gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     # get another name from the user
     name = $stdin.gets.chomp
   end
   # returns the array of students
-  students
+  @students
 end
 
 def interactive_menu
-students = []
 loop do
-  #1. print the menu and ask the user what to do
+  print_menu
+  process(gets.chomp)
+  end
+end
+
+def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "9. Exit" # 9 because we will be adding more items
-  #2. read the input and save it to a variable
-  selection = gets.chomp
-  #3. do what the user has asked
-    case selection
-    when "1"
-      students = input_statements
-    when "2"
-      print_header
-      print(students)
-      footer(students)
-    when "9"
-      exit
-    else
-      "I don't know what you meant, try again"
-    end
+end
+
+def show_students
+  print_header
+  print_student_list
+  footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    students = input_statements
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    "I don't know what you meant, try again"
   end
 end
 
