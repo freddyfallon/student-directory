69418878a0e9d888a9460718631c5305d258ca41
Freddy Fallon <freddyfallon@gmail.com>
2017-03-01 10:18:57 +0000

    Enabling directory.rb to take arguments from command line

--- directory.rb
+++ directory.rb
@@ -35,7 +35,7 @@ end
 def interactive_menu
 loop do
   print_menu
-  process(gets.chomp)
+  process($stdin.gets.chomp)
   end
 end
 
@@ -82,8 +82,8 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
@@ -91,4 +91,17 @@ def load_students
   file.close
 end
 
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}."
+  else
+    puts "Sorry, #{filename} doesnt' exist."
+    exit
+  end
+end
+
+try_load_students
 interactive_menu
